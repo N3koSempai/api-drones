5ac2927523253bf9c6858c32fa450682d982ce31
n3kosempai <alvarom12098@gmail.com>
2021-11-27 18:27:13 -0500

    added conditional cargo exist? drone empty?
    added conditionals for know is exist the cargo in other drone or this
    drone have cargo already and return a string with instructions

--- server_start.py
+++ server_start.py
@@ -25,22 +25,35 @@ def get(typedata = hug.types.text, data = hug.types.JSON): #handle the get petit
 @hug.http(typedata = hug.types.text, data = hug.types.JSON)
 def post(typedata , data):
     global count
+
     if typedata == 'insert_drone' and count <= 10:
         count = count + 1
         res = dbc.insert(typedata,data)
-
         return res
     elif typedata == 'insert_medication':
         res = dbc.insert(typedata,data)
         return res
+
     elif typedata == 'insert_cargo':
         res = dbc.get_data(typedata = "get_drone",data = data) #get if that drone have cargo 
-        
+        loaded = dbc.get_data(typedata = 'get_medication', data = data)
         #add here a system for try if that cargo is already loaded
-        if res[5] == 'None': #is if empty?
+        print(loaded)
+        if loaded == None or res == None:
+            return "you dont have this drone or medication register"
+        elif loaded[4] == 1:
+            return "this package is has already loaded. create a new medication package with other code"
+        elif res[5] == 'None' and loaded[4] == 0: #is if empty?
             print(res)
             res = dbc.insert(typedata,data) #set the serial of new cargo
-        return res
+            change_loaded = {}
+            change_loaded['loaded'] = 1
+            change_loaded['code'] = loaded[2]
+            change = dbc.insert(typedata='loaded',data = change_loaded)
+            return res
+        else:
+            return False
+
     elif typedata == 'test':
         return True
     return False
