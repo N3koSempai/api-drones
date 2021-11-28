6d26d76f65bfb2b96da506256d069321a62849ca
n3kosempai <alvarom12098@gmail.com>
2021-11-28 18:17:17 -0500

    change protocol to post and delete unecesary mth
    delete a unecesary method
    change the protocol in request to post request
    rename test_insert to test_db

--- test_.py
+++ test_.py
@@ -26,26 +26,20 @@ class TestClass(unittest.TestCase):
         status = battery_s.get_status() #take the actual status of the battery
         self.assertIn(status, battery_levels)
 
-    def test_insert(self): # for this test delete the db because duplication error with a Unique field inside the db
+    def test_db(self): # for this test delete the db because duplication error with a Unique field inside the db
 
         self.database.initial_state() #create the database if not exist for the full test
         self.assertTrue(self.database.insert(self.typedb, self.datadict))
     
-    #def test_get_data(self): #is not posible run this test with the test_insert_drone . run the other fist and after this
-    #   self.assertTrue(self.database.get_data(self.datajs))
-
-    #def test_delete_drone(self): #not compatible for now with test_insert_drone
-    #    self.assertTrue(self.database.delete(self.datajs))
-
     
 
     async def test_get_api_server_http(self):
         os.chdir(os.path.dirname(os.path.realpath(__file__)))
         task1 = os.system("hug -f server_start.py")
-        url = "http://127.0.0.1:8000/get"
+        url = "http://127.0.0.1:8000/post"
         await task1
         asyncio.sleep(1)
-        await self.assertTrue(requests.get(url, params=self.typedata,json = self.data))
+        await self.assertTrue(requests.post(url))
 
 
 
