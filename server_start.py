a4d829a4c8f0efa54456f109a091665e647c8c2a
n3kosempai <alvarom12098@gmail.com>
2021-11-29 18:59:02 -0500

    add status.error for False return

--- server_start.py
+++ server_start.py
@@ -1,7 +1,6 @@
 from falcon import http_error, status_codes
 import hug
 import json
-
 import falcon
 import threading
 from modules import connect_db
@@ -33,10 +32,10 @@ class Drone(object):
     @hug.post('/drone',output = hug.output_format.json, input = hug.input_format.json, )
     @hug.http(accept=('POST'))
     def drone(data: hug.types.json): #handle the petition
-        res = dbc.get_data(typedata = 'get_drone', data = data)
-        if res == None:
-            raise falcon.HTTPError(falcon.HTTP_404)
-        res = dict(zip(fields,res))
+        res = dbc.get_data(typedata = 'get_drone', data = data) #search in database
+        if res == None or res == False: #if dont have.
+            raise falcon.HTTPError(falcon.HTTP_404) #raise a error not found
+        res = dict(zip(fields,res)) #format with names of field before response
         return res
 
     @hug.post('/server_test',output = hug.output_format.json, input = hug.input_format.json )
@@ -48,15 +47,15 @@ class Drone(object):
     @hug.http(accept=('POST'))
     def medication(data: hug.types.json):
         res = dbc.get_data(typedata = 'get_medication', data =data)
-        if res == None:
+        if res == None or res == False:
             raise falcon.HTTPError(falcon.HTTP_404)
         return res
 
-    @hug.post('/available_drone',output = hug.output_format.json, input = hug.input_format.json)
-    @hug.http(accept=('POST'))
+    @hug.get('/available_drone',output = hug.output_format.json)
+    @hug.http(accept=('GET'))
     def available_drone(data: hug.types.json):
-        res = dbc.get_data(typedata = 'get_available_drone', data =data)
-        if res == None:
+        res = dbc.get_data(typedata = 'get_available_drone', data = 0)
+        if res == None or res == False:
             raise falcon.HTTPError(falcon.HTTP_404)
         return res
 
@@ -64,7 +63,7 @@ class Drone(object):
     @hug.http(accept=('POST'))
     def cheking_loading(data: hug.types.json):
         res = dbc.get_data(typedata = 'checking_loading', data =data)
-        if res == None:
+        if res == None or res == False:
             raise falcon.HTTPError(falcon.HTTP_404)
         return res
 
@@ -72,7 +71,7 @@ class Drone(object):
     @hug.http(accept=('POST'))
     def checking_battery(data: hug.types.json):
         res = dbc.get_data(typedata = 'checking_battery', data =data)
-        if res == None:
+        if res == None or res == False:
             raise falcon.HTTPError(falcon.HTTP_404)
         return res
 
@@ -94,7 +93,7 @@ class Drone(object):
         size = accept_model[temp] #search in dict the weigth 
         data['weigth'] = size #set data weigth in the data for registering in db
         res = dbc.insert(typedata = 'insert_drone',data = data) #call to insert the data in db
-        if res == None:
+        if res == None or res == False:
             raise falcon.HTTPError(falcon.HTTP_404)
         return res
 
@@ -106,12 +105,12 @@ class Drone(object):
             letters_name = i.isalpha() #is a letter
             number_name = i.isdigit() #is a digit
             if i == '-' or i == '_':
-                underscore = True
+                underscore_name = True
             if letters_name == True or number_name == True or underscore_name == True: #if is not invalid continue loops 
                 continue
             else:
-                res = 'bad format in the name of medication'
-                return res #is invalid format 
+                raise falcon.HTTPError(falcon.HTTP_404, title = 'bad format in the name of medication') 
+                
 
         for i in data['code']:#test format in code for medication item
             underscore_code = False
@@ -122,9 +121,7 @@ class Drone(object):
             if underscore_code == True or number_code == True or uppercase == True:
                 continue
             else:
-                res = 'bad format in the code of medication'
-                return res #is invalid format
-
+                raise falcon.HTTPError(falcon.HTTP_404, title = 'bad format in the code of medication') 
 
         res = dbc.insert(typedata = 'insert_medication',data = data) 
         return res
@@ -136,14 +133,14 @@ class Drone(object):
         loaded = dbc.get_data(typedata = 'get_medication', data = data)# try if that cargo is already loaded
     
         if loaded == None or res == None: #search if the drone and medication item exist
-            r = "you dont have this drone or medication register"
-            return r
-        elif loaded[4] == 1: #test if loaded already
-            r = "this package is has already loaded. create a new medication package with other code or chek this drone"
-            return r
+            raise falcon.HTTPError(falcon.HTTP_404, title = "you dont have this drone or medication register") 
+
+        elif loaded[4] == 1: #test if already loaded
+            raise falcon.HTTPError(falcon.HTTP_404, title = "this package is has already loaded. create a new medication package with other code or chek this drone") 
+
         elif res[3] < loaded[1]: #
-            r = "the weigth of the cargo is higher than the capacity of the drone"
-            return r
+            raise falcon.HTTPError(falcon.HTTP_404, title = "the weigth of the cargo is higher than the capacity of the drone")
+           
         elif res[5] == 'None' and loaded[4] == 0: #is if empty?
 
             res = dbc.insert(typedata = 'insert_cargo',data = data) #set the serial of new cargo
@@ -154,6 +151,3 @@ class Drone(object):
             return res
         else:
             raise falcon.HTTPError(falcon.HTTP_404)
-
-
-
